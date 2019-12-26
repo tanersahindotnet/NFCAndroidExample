07b473b0e5eecc1bdab118cf8d8cd51f636c1856
Taner Şahin <tanersahinn@windowslive.com>
2019-12-26 23:32:42 +0300

    Add project files.

---
+++ NFCAndroidExample/MainActivity.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using Android.App;
+using Android.Content;
+using Android.Nfc;
+using Android.Nfc.Tech;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace NFCAndroidExample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [IntentFilter(new[] { NfcAdapter.ActionNdefDiscovered, NfcAdapter.ActionTagDiscovered, Intent.CategoryDefault })]
+    public class MainActivity : AppCompatActivity
+    {
+        private NfcAdapter _nfcAdapter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            _nfcAdapter = NfcAdapter.GetDefaultAdapter(this);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            if (_nfcAdapter == null)
+            {
+                var alert = new Android.App.AlertDialog.Builder(this).Create();
+                alert.SetMessage("NFC is not supported on this device.");
+                alert.SetTitle("NFC Unavailable");
+                alert.Show();
+            }
+            else
+            {
+                var tagDetected = new IntentFilter(NfcAdapter.ActionTagDiscovered);
+                var ndefDetected = new IntentFilter(NfcAdapter.ActionNdefDiscovered);
+                var techDetected = new IntentFilter(NfcAdapter.ActionTechDiscovered);
+                var filters = new[] { ndefDetected, tagDetected, techDetected };
+
+                var intent = new Intent(this, GetType()).AddFlags(ActivityFlags.SingleTop);
+
+                var pendingIntent = PendingIntent.GetActivity(this, 0, intent, 0);
+
+                _nfcAdapter.EnableForegroundDispatch(this, pendingIntent, filters,
+                    new[]
+                    {new[] {
+                            Ndef.NfcForumType1
+                        },
+                        new[] {
+                            Ndef.NfcForumType2
+                        },
+                        new[] {
+                            Ndef.NfcForumType3
+                        },
+                        new[] {
+                            Ndef.NfcForumType4
+                        },
+                        new[] {
+                            Ndef.MifareClassic
+                        },
+                    }
+                );
+            }
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            if (intent.Action != NfcAdapter.ActionTagDiscovered) return;
+            var myTag = (Tag)intent.GetParcelableExtra(NfcAdapter.ExtraTag);
+            if (myTag == null) return;
+            var tagIdBytes = myTag.GetId();
+            var tagIdString = ByteArrayToString(tagIdBytes);
+            var reverseHex = LittleEndian(tagIdString);
+            var cardId = Convert.ToInt32(reverseHex, 16);
+            var alertMessage = new Android.App.AlertDialog.Builder(this).Create();
+            alertMessage.SetMessage("CardId:" + cardId);
+            alertMessage.Show();
+        }
+
+        private static string LittleEndian(string num)
+        {
+            var number = Convert.ToInt32(num, 16);
+            var bytes = BitConverter.GetBytes(number);
+            return bytes.Aggregate("", (current, b) => current + b.ToString("X2"));
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            var shb = new SoapHexBinary(ba);
+            return shb.ToString();
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            var id = item.ItemId;
+            return id == Resource.Id.action_settings || base.OnOptionsItemSelected(item);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}

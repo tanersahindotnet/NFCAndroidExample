885c5610121387240f945e4f51e1c39c25b2b960
Taner Şahin <tanersahinn@windowslive.com>
2020-01-26 12:00:52 +0300

    Nfc, Ndef message and WriteToTag added

--- NFCAndroidExample/MainActivity.cs
+++ NFCAndroidExample/MainActivity.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
 using Android.App;
 using Android.Content;
 using Android.Nfc;
+using Android.Nfc.Tech;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.V7.App;
@@ -72,6 +74,20 @@ namespace NFCAndroidExample
             var alertMessage = new Android.App.AlertDialog.Builder(this).Create();
             alertMessage.SetMessage("CardId:" + cardId); // Here's the id of card
             alertMessage.Show();
+
+            // Extra: Check if there's any Ndef message
+            var rawMessages = intent.GetParcelableArrayExtra(NfcAdapter.ExtraNdefMessages);
+            if (rawMessages == null) return;
+            var msg = (NdefMessage)rawMessages[0];
+            // Get NdefRecord which contains the actual data
+            var record = msg.GetRecords()[0];
+            if (record == null) return;
+            // The data is defined by the Record Type Definition (RTD) specification available from http://members.nfc-forum.org/specs/spec_list/
+            if (record.Tnf != NdefRecord.TnfWellKnown) return;
+            // Get the transmitted data
+            var data = Encoding.ASCII.GetString(record.GetPayload());
+            alertMessage.SetMessage("Data:" + data); // Here's the id of card
+            alertMessage.Show();
         }
 
         /// <summary>
@@ -85,6 +101,29 @@ namespace NFCAndroidExample
             var number = Convert.ToInt32(num, 16);
             var bytes = BitConverter.GetBytes(number);
             return bytes.Aggregate("", (current, b) => current + b.ToString("X2"));
+        }
+
+        /// <summary>
+        ///  Below a code snippet for writing to a discovered tag. This method can be called when a new intent (tag discovered) has been fired.
+        /// I'm using the Ndef class to the actual writing of the data to the tag.
+        /// The first step is to create the different parts payload, record, and the Ndef message which will be written on the tag.
+        /// Don't forget to call the Connect method before writing to the tag, otherwise, an exception will be thrown.
+        /// </summary>
+        /// <param name="intent"></param>
+        /// <param name="content"></param>
+        public void WriteToTag(Intent intent, string content)
+        {
+            if (!(intent.GetParcelableExtra(NfcAdapter.ExtraTag) is Tag tag)) return;
+            Ndef ndef = Ndef.Get(tag);
+            if (ndef == null || !ndef.IsWritable) return;
+            var payload = Encoding.ASCII.GetBytes(content);
+            var mimeBytes = Encoding.ASCII.GetBytes("text/plain");
+            var record = new NdefRecord(NdefRecord.TnfWellKnown, mimeBytes, new byte[0], payload);
+            var ndefMessage = new NdefMessage(new[] { record });
+
+            ndef.Connect();
+            ndef.WriteNdefMessage(ndefMessage);
+            ndef.Close();
         }
 
         public static string ByteArrayToString(byte[] ba)
